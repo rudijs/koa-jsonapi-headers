f8ecf484747395503ef0f929752c1d17e3c67b65
Rudi Starcevic <ooly.me@gmail.com>
2014-09-14 10:33:57 +0800

    Add header validation exclude feature, Readme updates

--- README.md
+++ README.md
@@ -66,6 +66,8 @@ Here's an example of manual catch and re-throw:
 
     app.use(koaJsonApiHeaders());
 
+If you have an API endpoint you wish to not use JSON API headers for you can add jsonapi=false to the URL query string.
+
 ## Tests with code coverage report in `test/coverage`
 
 Note: Requires nodes at least v0.11.13 (earlier v0.11 versions may work, have not checked for this).
--- test/koa-jsonapi-headers.spec.js
+++ test/koa-jsonapi-headers.spec.js
@@ -14,63 +14,6 @@ describe('JSON API Headers Middleware', function () {
     done();
   });
 
-  //describe('accept', function () {
-  //
-  //  it('should log request and response', function (done) {
-  //
-  //    app.use(koaJsonLogger());
-  //
-  //    // default route for test
-  //    app.use(function *route1(next) {
-  //      this.body = 'Test Response is OK.';
-  //      yield next;
-  //    });
-  //
-  //    request(app.listen())
-  //      .get('/')
-  //      .expect(200)
-  //      .end(function (err, res) {
-  //        if (err) {
-  //          should.not.exist(err);
-  //          return done(err);
-  //        }
-  //
-  //        // test http response
-  //        res.text.should.equal('Test Response is OK.');
-  //
-  //        // read in log file entry
-  //        fs.readFile('log/app.log', function (err, data) {
-  //          if (err) {
-  //            throw err;
-  //          }
-  //
-  //          // test JSON parsed log entry
-  //          var logEntry = JSON.parse(data.toString());
-  //
-  //          // bunyan property logging
-  //          logEntry.name.should.equal('app');
-  //          should.exist(logEntry.uid);
-  //          logEntry.uid.should.match(/^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i);
-  //
-  //          // request logging
-  //          logEntry.req.method.should.equal('GET');
-  //          logEntry.req.url.should.equal('/');
-  //          should.exist(logEntry.req.headers);
-  //
-  //          // response logging
-  //          logEntry.res.statusCode.should.equal(200);
-  //          should.exist(logEntry.res.responseTime);
-  //          logEntry.res.headers['x-powered-by'].should.equal('koa');
-  //
-  //          done();
-  //        });
-  //
-  //      });
-  //
-  //  });
-  //
-  //});
-
   describe('reject', function () {
 
     it('missing Content-type header', function (done) {
@@ -169,6 +112,30 @@ describe('JSON API Headers Middleware', function () {
             return done(err);
           }
           res.text.should.equal('Corrent headers found');
+          done();
+        });
+
+    });
+
+    it('should permit exclusions', function (done) {
+
+      app.use(koaJsonApiHeaders());
+
+      // default route
+      app.use(function *route1(next) {
+        this.body = 'Excluded headers request OK';
+        yield next;
+      });
+
+      request(app.listen())
+        .get('/?jsonapi=false')
+        .expect(200)
+        .end(function (err, res) {
+          if (err) {
+            should.not.exist(err);
+            return done(err);
+          }
+          res.text.should.equal('Excluded headers request OK');
           done();
         });
 
